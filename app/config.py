244c16c8082d3be133af699b5b1344939561b12d
TiagoMadeira <tiagopmadeira@gmail.com>
2026-02-23 18:40:59 +0000

    fix: added default valeu for auth_service_url setting

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    AUTH_SERVICE_URL : str
+    AUTH_SERVICE_URL : str = "http://127.0.0.1:8000"
     class Config:
         env_file = f".env.local"
         
